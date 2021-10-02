972094c8791b70936aed2defeb316b004f96a14b
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-02 13:00:57 +0900

    commit: (해시) 위장 (함수형 프로그래밍)

--- src/main/java/com/example/algorithm/programmers/Hash/Disguise.java
+++ src/main/java/com/example/algorithm/programmers/Hash/Disguise.java
@@ -1,33 +1,17 @@
 package com.example.algorithm.programmers.Hash;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import static java.util.stream.Collectors.*;
 
 /**
  * 프로그래머스 위장
- * for문 + hashmap
+ * solution2. 함수형 프로그래밍
 * */
 public class Disguise {
     public int solution(String[][] clothes) {
-        int answer = 1;
-
-        // 1. 맵에 넣기
-        HashMap<String,Integer> cntPerClothes = new HashMap<>();
-
-        for (String[] clothe : clothes) {
-            String kind = clothe[1];
-            int count = cntPerClothes.getOrDefault(kind, 0);
-
-            cntPerClothes.put(kind, count + 1);
-        }
-
-        // 2. 경우의 수 계산
-        for (Integer cnt: cntPerClothes.values()) {
-            answer = answer*(cnt+1); // 특정 옷 안 입었을 경우 고려하여 +1
-        }
-
-        // 3. 아무것도 입지 않았을 경우는 제외
-        answer = answer - 1;
-
-        return answer;
+       return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[1], counting())))
+                .values()
+                .stream().reduce(1L, (x, y) -> x*(y+1)).intValue() - 1;
     }
 }
