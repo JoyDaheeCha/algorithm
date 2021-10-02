8e56e1c1bcd6089be62f1cd07ff23426744ccace
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-02 12:38:29 +0900

    commit: (해시) 위장 (for+hash 풀이)

---
+++ src/test/java/com/example/algorithm/programmers/Hash/DisguiseTest.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.programmers.Hash;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DisguiseTest {
+
+    @Test
+    void test1() {
+        //given
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        //when
+        Disguise disguise = new Disguise();
+        int answer = 5;
+
+        //then
+        assertThat(disguise.solution(clothes)).isEqualTo(answer);
+    }
+
+    @Test
+    void test2() {
+        //given
+        String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
+        //when
+        Disguise disguise = new Disguise();
+        int answer = 3;
+
+        //then
+        assertThat(disguise.solution(clothes)).isEqualTo(answer);
+    }
+
+}
