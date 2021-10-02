9fd93e700e37574342dcc9f76752859c9bb7326b
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-02 15:06:02 +0900

    fix: (정렬) 가장 큰 수
    1. 3, 30 포함한 경우 330 리턴 (test case2)
    2. 0으로만 이루어진 array를 받을 경우 0리턴 (test case3)

--- src/main/java/com/example/algorithm/programmers/sort/BiggestNum.java
+++ src/main/java/com/example/algorithm/programmers/sort/BiggestNum.java
@@ -1,7 +1,6 @@
 package com.example.algorithm.programmers.sort;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,10 +11,17 @@ import java.util.stream.Collectors;
 * */
 public class BiggestNum {
     public String solution(int[] numbers) {
+
+        // 1. 내림 차순 sorting
         List<String> parsed = Arrays.stream(numbers)
                                     .mapToObj(Integer::toString)
-                                    .sorted(Collections.reverseOrder())
+                                    .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
                                     .collect(Collectors.toList());
+
+        // 2. 가장 큰 수가 0인 경우: 나머지도 0일것이므로, 0리턴
+        if (parsed.get(0).equals("0")) {
+            return "0";
+        }
 
         return String.join("", parsed);
     }
--- src/test/java/com/example/algorithm/programmers/sort/BiggestNumTest.java
+++ src/test/java/com/example/algorithm/programmers/sort/BiggestNumTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BiggestNumTest {
 
     @Test
-    void test1() {
+    void test_6_10_2() {
         //given
         int[] array = {6, 10, 2};
         String answer = "6210";
@@ -18,10 +18,21 @@ class BiggestNumTest {
     }
 
     @Test
-    void test2() {
+    void test_3_30_34_5_9(){
         //given
         int[] array = {3, 30, 34, 5, 9};
         String answer = "9534330";
+
+        //when
+        BiggestNum biggestNum = new BiggestNum();
+        assertThat(biggestNum.solution(array)).isEqualTo(answer);
+    }
+
+    @Test
+    void test_0_0_0() {
+        //given
+        int[] array = {0,0,0};
+        String answer = "0";
 
         //when
         BiggestNum biggestNum = new BiggestNum();
