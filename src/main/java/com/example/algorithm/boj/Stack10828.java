dc9943d0908f20681fe7712d3772219e7b144fa4
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-06 13:03:37 +0900

    refactor: (stack) 백준 10828 input, output 주석으로 추가

--- src/main/java/com/example/algorithm/boj/Stack10828.java
+++ src/main/java/com/example/algorithm/boj/Stack10828.java
@@ -3,6 +3,51 @@ package com.example.algorithm.boj;
 import java.io.*;
 import java.util.Stack;
 
+/**
+ * 백준 10828 스택
+ *
+ * 기억할 점: Scanenr보다는 BufferedReader가 빠르다.
+ * 관련 URL : https://st-lab.tistory.com/175 (스택 설명 자료)
+* */
+
+
+/**
+* push X: 정수 X를 스택에 넣는 연산이다.
+ * pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ * size: 스택에 들어있는 정수의 개수를 출력한다.
+ * empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+ * top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ *
+ * <input>
+ * 14
+ * push 1
+ * push 2
+ * top
+ * size
+ * empty
+ * pop
+ * pop
+ * pop
+ * size
+ * empty
+ * pop
+ * push 3
+ * empty
+ * top
+ *
+ * <output>
+ * 2
+ * 2
+ * 0
+ * 2
+ * 1
+ * -1
+ * 0
+ * 1
+ * -1
+ * 0
+ * 3
+* */
 public class Stack10828 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
