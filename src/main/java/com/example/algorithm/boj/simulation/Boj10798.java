c7b4509dd5952388ef6d6de89bc68ba664e261fa
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-07 10:34:24 +0900

    commit: (백준 - 구현) 10798 세로 읽기

--- src/main/java/com/example/algorithm/boj/simulation/Boj10798.java
+++ src/main/java/com/example/algorithm/boj/simulation/Boj10798.java
@@ -3,8 +3,6 @@ package com.example.algorithm.boj.simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
 
 public class Boj10798 {
 
@@ -13,21 +11,21 @@ public class Boj10798 {
         int wordLen = 26;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[][] words = new String[wordLen][sentneceCount];
+        String[][] words = new String[sentneceCount][wordLen];
 
         for (int i = 0; i < sentneceCount; i++) {
-            Arrays.fill(words[i], "");
-        }
-
-        for (int i = 0; i < sentneceCount; i++) {
-            words[i] = br.readLine().split("");
+            String[] word = br.readLine().split("");
+            System.arraycopy(word, 0, words[i], 0, word.length);
         }
 
         StringBuilder sb = new StringBuilder();
 
         for (int j = 0; j < wordLen; j++) {
             for (int k = 0; k < sentneceCount; k++) {
-                sb.append(words[j][k]);
+                String current = words[k][j];
+                if(current != null) {
+                    sb.append(words[k][j]);
+                }
             }
         }
 
