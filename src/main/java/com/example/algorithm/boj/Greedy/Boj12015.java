f4636435d315bd292ebc994f72c6d7c61eb66c46
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-08 18:38:58 +0900

    commit: (백준 - Greedy) 12015 가장 긴 증가하는 부분 수열 (NlogN으로 풀이)

--- src/main/java/com/example/algorithm/boj/Greedy/Boj12015.java
+++ src/main/java/com/example/algorithm/boj/Greedy/Boj12015.java
@@ -1,30 +1,52 @@
 package com.example.algorithm.boj.Greedy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * 백준 12015 가장 긴 증가하는 부분 수열 2
+ *
+ * 요구사항:
+ * 배열의 입력 순서를 유지
+ * 오름차순
+ * 가장 긴 수열 찾기
+ *
+ * 설명: https://dragon-h.tistory.com/2
 * */
 public class Boj12015 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
-        Integer[] nums = new Integer[n];
+        int n = sc.nextInt();
+        int[] nums = new int[n]; // 입력 받을 수열
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        list.add(0); // 배열의 첫 요소와 비교하기
+
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            nums[i] = sc.nextInt();
         }
 
-        Set<Integer> uniqueNums = new HashSet<>(Arrays.asList(nums));
+        for (int i = 0; i < n; i++) {
+            int current = nums[i];
 
-        System.out.println(uniqueNums.size());
+            if(current > list.get(list.size()-1)) {
+                list.add(current);
+            }
+            else{
+                int left = 0;
+                int right = list.size() - 1;
+                while(left < right) {
+                    int mid = (left + right) / 2;
+                    if(list.get(mid) >= current) {
+                        right = mid;
+                    }else{
+                        left = mid + 1;
+                    }
+                }
+                list.set(right, current);
+            }
+        }
+        System.out.println(list.size()-1);
     }
 }
