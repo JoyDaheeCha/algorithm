b0b03bb43480a0abf8e45610813c446e6fe46469
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-08 21:28:25 +0900

    commit: (백준 - Greedy) 1744 수묶기 (틀렸다고 나옴)

--- src/main/java/com/example/algorithm/boj/Boj711/Boj1744.java
+++ src/main/java/com/example/algorithm/boj/Boj711/Boj1744.java
@@ -1,64 +1,61 @@
 package com.example.algorithm.boj.Boj711;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
  * 백준 1744 / 수 묶기
  *
- * return은 프로그램 종료고, break는 while문 탈출임! 이런 기초적인 실수는 하지말자.
+ * 1. return은 프로그램 종료고, break는 while문 탈출임! 이런 기초적인 실수는 하지말자.
+ * 2. 양수는 큰 수 끼리 묶을수록 이득이지만, 음수는 작은 수끼리 묶을 수록 이득이다.
 * */
 public class Boj1744 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        boolean hasZero = false;
         int n = sc.nextInt();
 
-        // 1. 배열 값 내림차순 정렬
-        PriorityQueue<Integer> queue = new PriorityQueue<>((x,y) -> Integer.compare(y,x));
+        // 1. 배열 값 정렬
+        PriorityQueue<Integer> positive = new PriorityQueue<>((x,y) -> Integer.compare(y,x)); // 내림 차순 정렬 e.g. 3,2,1
+        PriorityQueue<Integer> negative = new PriorityQueue<>(); // 오름 차순 정렬 -3,-2,-1
 
         for (int i = 0; i < n; i++) {
-            queue.add(sc.nextInt());
-        }
-
-        List<Integer> calculated = new ArrayList<>();
-
-        while(!queue.isEmpty()) {
-            // 현재 값
-            int current = queue.poll();
-
-            // 다음 값 없을 경우 현재 값만 넣고 while 문 종료
-            if(queue.peek() == null) {
-                calculated.add(current);
-                break;
-            }
-
-            // 1. 현재 양수 일 때
-            if (current > 0) {
-                if(queue.peek() > 0) {
-                    calculated.add(current * queue.poll());
-                }else {
-                    calculated.add(current);
-                }
-            }
-            // 2. 현재 0일 때
-            else if (current == 0) {
-                if(queue.size() / 2 == 0) {
-                    calculated.add(current);
-                }else {
-                    calculated.add(current * queue.poll());
-                }
-            }
-            // 3. 현재 음수 일때
-            else {
-                calculated.add(current * queue.poll());
+            int number = sc.nextInt();
+            if (number > 0) {
+                positive.add(number);
+            } else if (number == 0) {
+                hasZero = true;
+            } else {
+                negative.add(number);
             }
         }
 
-        // 4. 최댓값 구하기
-        int maxSum = calculated.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(maxSum);
+        //2. 양수 값 덧셈 (묶거나 묶지 않거나)
+        int posSum = 0; // 양의 정수 계산 값
+        while(!positive.isEmpty()) {
+            int current = positive.poll();
+
+            if(positive.peek() == null) {
+                posSum += current; // 뒤 elment가 없으면 본인만 더한다.
+            } else {
+                posSum = current * positive.poll(); // 뒤 엘리먼트와 본인을 곱한 후, 더한다.
+            }
+        }
+
+        //3. 음수 값 덧셈 (묶거나 묶지 않거나)
+        int negSum = 0;
+        while(!negative.isEmpty()) {
+            int current = negative.poll();
+
+            if(negative.peek() == null) {
+                if(!hasZero) {
+                    negSum += current; // 0을 가진 경우, 0과 곱하여 없앨 수 있다.
+                }
+            } else {
+                negSum = current * negative.poll();
+            }
+        }
+
+        System.out.println(posSum + negSum);
     }
 }
