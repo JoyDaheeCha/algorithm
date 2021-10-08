0af245dae239e3d131372c34ec30efbe16dc44dd
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-08 16:08:35 +0900

    commit: (백준 - Greedy) 2019 순회강연 (테스트 케이스 통과)
    1. ~일에 강의해주세요 가 아니라 ~일 안에만 강의해주세요 임 주의
    
    테스트 케이스 예시
    3
    1  1
    10 2
    10 2

--- src/main/java/com/example/algorithm/boj/Greedy/Boj2109.java
+++ src/main/java/com/example/algorithm/boj/Greedy/Boj2109.java
@@ -6,6 +6,19 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 / Greedy / 2109 / 순회강연
+ *
+ * 솔루션 : 가장 비싼 강의를 우선으로, 그리고 최대한 늦게 강연한다.
+ *
+ * [주의할 테스트 케이스]
+ * 3
+ * 1  1
+ * 10 2
+ * 10 2
+ * 위 경우에는 2일에 10, 1일에 10 강의가 유리하다
+ * (출처: https://www.acmicpc.net/board/view/1885)
+* */
 public class Boj2109 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,18 +33,25 @@ public class Boj2109 {
         }
 
         Arrays.sort(pd, (o1, o2) -> {
-            if(o1[1] == o2[1]) {
-                return Integer.compare(o2[0],o1[0]);
+            if(o1[0] == o2[0]) {
+                return Integer.compare(o2[1],o1[1]);
             }
-            return Integer.compare(o1[1], o2[1]);
-        }); // 일 오름 차순, 가격 내림 차순 정렬
+            return Integer.compare(o2[0], o1[0]);
+        }); // 가격 내림 차순, 날짜 내림 차순
+
+        int latestD = Arrays.stream(pd).mapToInt(item -> item[1]).max().orElse(-1);
+
+        boolean[] booked = new boolean[latestD+1]; // d의 범위가 작아서 (0~10,000) 그냥 10001로 둬도 됨.
+
 
         long totalIncome = 0;
-        int curDay = 0;
         for (int i = 0; i < n; i++) {
-            if(pd[i][1] > curDay) {
-                curDay = pd[i][1];
-                totalIncome += pd[i][0];
+            for (int j = pd[i][1]; j >= 1; j--) {
+                if(!booked[j]) {
+                    booked[j] = true; // 강의 스케쥴 잡음
+                    totalIncome+= pd[i][0];
+                    break;
+                }
             }
         }
         System.out.println(totalIncome);
