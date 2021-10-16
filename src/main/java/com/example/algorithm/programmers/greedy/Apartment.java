1b7abd40bdca211bfdcdddd1e33459e26eaba05c
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-16 10:41:45 +0900

    commit: 프로그래머스/그리디/기지국

--- src/main/java/com/example/algorithm/programmers/greedy/Apartment.java
+++ src/main/java/com/example/algorithm/programmers/greedy/Apartment.java
@@ -3,48 +3,58 @@ package com.example.algorithm.programmers.greedy;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 프로그래머스 / 그리디 / 기지국
+ * N: 200,000,000 이므로 배열 선언하지 말것.
+ *
+ * 블로그 설명: https://tosuccess.tistory.com/88
+* */
 public class Apartment {
     public static void main(String[] args) {
+        //System.out.println((solution(20, new int[]{4,11}, 2))); // 기댓값 4
         System.out.println((solution(16, new int[]{9}, 2))); // 기댓값 3
         //System.out.println((solution(11, new int[]{4,11}, 1))); // 기댓값 3
     }
+
     public static int solution(int n, int[] stations, int w) {
         // 1. 한 기지국의 영향 범위 구하기
-        int eLen = 1 + 2*w;
+        int eLen = 1 + 2*w; // W는 자연수
 
-        int[][] effected = new int[stations.length+1][2];
-        effected[0] = new int[]{0,0};
+        int left = 0;
+        int right = 0;
 
-        for (int i = 1; i < effected.length; i++) {
-            effected[i] = new int[]{stations[i-1]-w, stations[i-1]+w};
+        int baseL = 0;
+        int baseR = 0;
+
+        int count = 0; // 추가로 설치할 기지국 수
+        for (int i = 0; i < stations.length; i++) {
+            int cur = stations[i];
+
+            left = baseR + 1;
+
+            baseL = cur - w;
+            baseR = cur + w;
+
+            if(baseL < 0) baseL = 0;
+            if(baseR > n) baseR = n;
+
+            right = baseL - 1;
+
+            if(right < 0) continue;
+            if(right < left) continue;
+
+            double gap = (double) (right - left + 1) / eLen;
+            if(gap % 1 == 0) count += (int) gap;
+            else count += (int) gap + 1;
         }
 
-        //2. 영향 받지 않는 아파트 그룹 넓이
-        List<Integer> noEffectZone = new LinkedList<>();
-
-        for (int i = 1; i < effected.length; i++) {
-            noEffectZone.add(effected[i][0]-effected[i-1][1]-1);
+        // 모두 다 커버되지 않았을 경우
+        if(baseR != n) {
+            double gap = (double) (n - (baseR+1) + 1) / eLen;
+            if(gap % 1 == 0) count += (int) gap;
+            else count += (int) gap + 1;
         }
 
-        //3. 각 그룹별 기지국 수 구한 후, 전체 기지국 수 구하기
-        int totalCnt = 0;
-        for (int len : noEffectZone) {
-            if(len <= eLen) {
-                totalCnt += 1;
-            } else {
-                totalCnt += len / eLen + 1;
-            }
-        }
-
-        if(n > effected[effected.length-1][1]) {
-            int finalGap = n - effected[effected.length-1][1];
-            if(finalGap <= eLen) {
-                totalCnt += 1;
-            } else {
-                totalCnt += finalGap / eLen + 1;
-            }
-        }
-
-        return totalCnt;
+        return count;
     }
 }
