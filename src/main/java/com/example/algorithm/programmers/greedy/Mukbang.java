31d56ae5f6eeb9c0bb8639fdd83cb3883bcc1490
JoyDaheeCha <daheecha0612@gmail.com>
2021-10-24 20:42:10 +0900

    commit: 프로그래머스/greedy4/무지의 먹방라이브 (실패함)
    1. long과 int를 써야할 때 정확히 알지 못함.

---
+++ src/main/java/com/example/algorithm/programmers/greedy/Mukbang.java
@@ -0,0 +1,75 @@
+package com.example.algorithm.programmers.greedy;
+
+import com.example.algorithm.nadongbin.Greedy6;
+
+import java.util.*;
+
+/**
+ * 무지의 먹방라이브: https://programmers.co.kr/learn/courses/30/lessons/42891
+ * 그리디 lv4. 
+ * 강의 : https://www.youtube.com/watch?v=4MWxAt4fx5I&t=74s
+* */
+public class Mukbang {
+    class Food {
+        private int time;
+        private int idx;
+
+        public Food(int time, int idx) {
+            this.time = time;
+            this.idx = idx;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public int getIdx() {
+            return idx;
+        }
+    }
+
+    Comparator<Food> byTime = new Comparator<Food>() {
+        @Override
+        public int compare(Food o1, Food o2) {
+            return o1.time - o2.time;
+        }
+    };
+
+    Comparator<Food> byIdx = new Comparator<Food>() {
+        @Override
+        public int compare(Food o1, Food o2) {
+            return o1.idx - o2.idx;
+        }
+    };
+
+
+    public int solution(int[] food_times, long k) {
+        List<Food> foods = new ArrayList<Food>();
+
+        // 1. 시간 순 내림 차순 정렬
+        for(int i=0; i<food_times.length; i++) {
+            foods.add(new Food(food_times[i], i+1)); // 1번 음식 ~ N번 음식
+        }
+
+        foods.sort(byTime);
+
+        int sum = 0; // 지금까지 사용한 총 시간
+        int prev = 0; //직전에 다 먹은 음식의 소요시간
+        int length = foods.size(); //덜 먹고 남아있는 음식 갯수
+
+        for (int j=0; j<foods.size(); j++) {
+            long diff = foods.get(j).getTime() - prev;
+            if(sum + diff * length <= k) {
+                sum += diff * length;
+                prev = foods.get(j).getTime();
+                length--;
+            }else {
+                int idx = ((int)k - sum) % length;
+                foods.subList(j, foods.size()).sort(byIdx);
+                return foods.get(j+idx).getIdx();
+            }
+        }
+
+        return -1;
+    }
+}
